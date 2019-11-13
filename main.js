f285995fb663db54c52ec49ec37d4d97b0515fef
meon <meon77@ukr.net>
2019-11-13 16:36:13 +0200

    timer

--- main.js
+++ main.js
@@ -33,4 +33,54 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    //timer
+
+    let deadline = '2019-11-14';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60), 
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+        // hours = Math.floor((t/1000/60/60) % 24)
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function Zero(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
+
+            hours.textContent = Zero(t.hours);
+            minutes.textContent = Zero(t.minutes);
+            seconds.textContent = Zero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
